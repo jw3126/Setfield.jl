8da84a0db22a7b915a3b8c9ed5303fa39e1417a3
Takafumi Arakaki <aka.tkf@gmail.com>
2019-10-01 18:36:59 -0700

    Unsupport dot calls in index

--- src/lowering.jl
+++ src/lowering.jl
@@ -41,48 +41,15 @@ lower_indices(lower, collection, indices) =
     map(index -> lower_index(lower, collection, index), indices)
 
 function lower_index(lower, collection, index)
-    ex = handle_dotcall(index) do ex
-        lower_index(lower, collection, ex)
-    end
-    ex === nothing || return something(ex)
-
+    # GroundEffects handles dot calls here but we don't need to do
+    # this because `setindex(::Tuple, ::Tuple, ::Tuple)` is not
+    # supported.
     if isexpr(index, :call)
         return Expr(:call, lower_indices(lower, collection, index.args)...)
     elseif index === :end
         return :($(Base.lastindex)($collection))
     end
     return lower(index)
-end
-
-function isdotopcall(ex)
-    ex isa Expr && !isempty(ex.args) || return false
-    op = ex.args[1]
-    return op isa Symbol && Base.isoperator(op) && startswith(string(op), ".")
-end
-
-isdotcall(ex) =
-    isexpr(ex, :.) && length(ex.args) == 2 && isexpr(ex.args[2], :tuple)
-
-function handle_dotcall(lower, ex)
-    isdotcall(ex) || isdotopcall(ex) || return nothing
-    return Expr(:call, Base.materialize, handle_lazy_dotcall(lower, ex))
-end
-
-function handle_lazy_dotcall(lower, ex)
-    if isdotcall(ex)
-        args = [
-            lower(ex.args[1])
-            map(x -> handle_lazy_dotcall(lower, x), ex.args[2].args)
-        ]
-    elseif isdotopcall(ex)
-        args = [
-            Symbol(String(ex.args[1])[2:end])
-            map(x -> handle_lazy_dotcall(lower, x), ex.args[2:end])
-        ]
-    else
-        return lower(ex)
-    end
-    return Expr(:call, Base.broadcasted, args...)
 end
 
 end  # module
