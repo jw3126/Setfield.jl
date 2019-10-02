7d7ba154ba79f81fc1ecb1dd8b96f3100b7efd4a
Takafumi Arakaki <aka.tkf@gmail.com>
2019-10-01 18:44:18 -0700

    Simplify src/lowering.jl using the assumption of Setfield usage

--- src/lowering.jl
+++ src/lowering.jl
@@ -11,30 +11,9 @@ lower(ex) = something(handle_ref(lower, ex), ex)
 
 function handle_ref(lower, ex)
     isexpr(ex, :ref) || return nothing
-    statements, collection, indices = _handle_ref(lower, ex)
-    push!(statements, Expr(:call, Base.getindex, collection, indices...))
-    if length(statements) == 1
-        return statements[1]
-    else
-        return Expr(:block, statements...)
-    end
-end
-
-function _handle_ref(lower, ex)
-    statements = []
-    if length(ex.args) == 1
-        collection = ex.args[1]
-        indices = []
-    else
-        if ex.args[1] isa Symbol
-            collection = ex.args[1]
-        else
-            @gensym collection
-            push!(statements, :($collection = $(ex.args[1])))
-        end
-        indices = lower_indices(lower, collection, ex.args[2:end])
-    end
-    return statements, collection, indices
+    collection = ex.args[1] :: Symbol
+    indices = lower_indices(lower, collection, ex.args[2:end])
+    return Expr(:call, Base.getindex, collection, indices...)
 end
 
 lower_indices(lower, collection, indices) =
